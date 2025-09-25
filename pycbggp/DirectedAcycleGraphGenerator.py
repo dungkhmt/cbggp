7028f2bd30b8e60c2f346f433844bed175d7a18b
giapnguyen <giapnguyen2004@gmail.com>
2025-09-25 20:38:56 +0700

    add DAG Generator

--- pycbggp/DirectedAcycleGraphGenerator.py
+++ pycbggp/DirectedAcycleGraphGenerator.py
@@ -1,6 +1,5 @@
 from CBSGG import Graph
-from CBSGG import Node 
-from CBSGG import Edge
+import random
 
 '''
     TODO by Nguyen Minh Giap
@@ -9,5 +8,43 @@ from CBSGG import Edge
 class DirectedAcycleGraphGenerator:
  #generate randomly a directed acycle graph with n nodes and m edges
  def Generate(self, nbNodes, nbEdges):
-  # return a directed acyclic graph with nbNodes nodes, nbEdges edges 
-  return None
+  """
+  Generate a random Directed Acycle Graph with nbNodes nodes, nbEdges edges
+  
+  Args:
+    nbNodes: The number of nodes in the graph
+    nbEdges: The number of edges in the graph
+    
+  Returns:
+    A directed acycle graph if successful, None otherwise
+  """
+  
+  if nbNodes < 0:
+    print("Invalid number of nodes.")
+    return None
+  if nbEdges < 0 or nbEdges > nbNodes * (nbNodes - 1) // 2:
+    print("Invalid number of edges.")
+    return None
+  
+  G = Graph(nbNodes)
+  
+  topo_order = list(range(nbNodes))
+  random.shuffle(topo_order)
+  edges = set()
+  
+  while len(edges) < nbEdges:
+      i, j = random.sample(range(nbNodes), 2)
+      if i > j:
+          i, j = j, i
+      u, v = topo_order[i], topo_order[j]
+      if (u, v) not in edges:
+        edges.add((u, v))
+        G.AddEdge(u, v)
+
+  return G
+
+DAG = DirectedAcycleGraphGenerator()
+G = DAG.Generate(5, 4)
+if G != None:
+    G.Print()
+    G.PrintUnweighted()
